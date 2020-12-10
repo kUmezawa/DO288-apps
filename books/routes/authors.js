61a93a1b66976a142c1785bb7cfa64ab252bd4f3
Student User <student@workstation.lab.example.com>
2020-12-09 22:01:28 -0500

    edit lint

--- books/routes/authors.js
+++ books/routes/authors.js
@@ -1,8 +1,12 @@
 const express = require('express');
+
 const router = express.Router();
+
 const authors = require('../data/authors');
+
 /* GET users listing. */
 router.get('/', (req, res) => {
   res.json(authors);
 });
+
 module.exports = router;
